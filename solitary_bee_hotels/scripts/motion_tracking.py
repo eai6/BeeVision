7e2456c3b09c864608aac235cbe64ad9b6068856
eai6 <edwardamoahidun@gmail.com>
2024-01-09 16:24:18 -0500

    tracking algorithm

--- solitary_bee_hotels/scripts/motion_tracking.py
+++ solitary_bee_hotels/scripts/motion_tracking.py
@@ -2,136 +2,163 @@ import cv2
 from ultralytics import YOLO
 import numpy as np
 import pandas as pd
-
-# Load the YOLOv8 model
-model = YOLO('models/bee_detection_model.pt')
-
-# Replace with video path that you want to do motion tracking on
-video5 = "/Users/edwardamoah/Documents/GitHub/BeeVision/solitary_bee_hotels/datasets/videos/annotated/2023-09-08_10_10_01.mp4"
-cap = cv2.VideoCapture(video5)
+import argparse
 
 
-# Read the first frame
-ret, prev_frame = cap.read()
+def main(video, persist, filename):
+        
+    # Load the YOLOv8 model
+    model = YOLO('models/bee_detection_model.pt')
 
-# Convert the frame to grayscale
-prev_frame_gray = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)
+    # Replace with video path that you want to do motion tracking on
+    #video5 = "/Users/edwardamoah/Documents/GitHub/BeeVision/solitary_bee_hotels/datasets/videos/annotated/2023-09-08_10_10_01.mp4"
+    cap = cv2.VideoCapture(video)
 
-def update_background(current_frame, prev_bg, alpha):
-    bg = alpha * current_frame + (1 - alpha) * prev_bg
-    bg = np.uint8(bg)  
-    return bg
 
-frame_counter = 0
-frames = []
-motions_cords = []
-frame_motions = []
-detections_cords = []
-frame_detections = []
-while cap.isOpened():
+    # Read the first frame
+    ret, prev_frame = cap.read()
 
-    frame_counter += 1
-    # Read the next frame
-
-    ret, current_frame = cap.read()
-    
-    if not ret:
-        break
-
-    frames.append(frame_counter)
-    
     # Convert the frame to grayscale
-    current_frame_gray = cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY)
-    
-    # Calculate the absolute difference between the current frame and the previous frame
-    frame_diff = cv2.absdiff(current_frame_gray, prev_frame_gray)
-    
-    # Apply thresholding to the difference frame
-    _, thresholded_frame = cv2.threshold(frame_diff, 10, 255, cv2.THRESH_BINARY)
-    
-    # Find contours in the thresholded frame
-    contours, _ = cv2.findContours(thresholded_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    
-    # Draw rectangles around moving objects
-    frame_contours = []
-    for contour in contours:
-        if cv2.contourArea(contour) > 200:  # You can adjust this threshold based on your specific case
-            (x, y, w, h) = cv2.boundingRect(contour)
-            cv2.rectangle(current_frame, (x, y), (x + w, y + h), (255,0, 0), 2)
-            frame_contours.append((x, y, w, h))
+    prev_frame_gray = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)
 
-    # find average of all contours
-    if len(frame_contours) > 0:
-        x = np.mean([x for (x, y, w, h) in frame_contours])
-        y = np.mean([y for (x, y, w, h) in frame_contours])
-        w = np.mean([w for (x, y, w, h) in frame_contours])
-        h = np.mean([h for (x, y, w, h) in frame_contours])
-        motions_cords.append((x, y, w, h))
-        frame_motions.append(frame_contours)
-        cv2.rectangle(current_frame, (int(x), int(y)), (int(x) + int(w), int(y) + int(h)), (0, 255, 0), 2)
-    else: # no motion detection
-        motions_cords.append((0, 0, 0, 0))
-        frame_motions.append([])
-               
+    def update_background(current_frame, prev_bg, alpha):
+        bg = alpha * current_frame + (1 - alpha) * prev_bg
+        bg = np.uint8(bg)  
+        return bg
 
-    # Run YOLOv8 inference on the frame if motion is detected
-    if len(frame_contours) > 0:
-        results = model.track(current_frame,persist=False)
+    frame_counter = 0
+    frames = []
+    motions_cords = []
+    frame_motions = []
+    detections_cords = []
+    frame_detections = []
+    class_ids = []
 
-        # annotated frame from model
-        annotated_frame = results[0].plot()
+    while cap.isOpened():
 
-        # get detection 
-        boxes = results[0].boxes.xywh.tolist()
+        frame_counter += 1
+        # Read the next frame
 
-        # get detection cords
-        model_detections = [(x,y,w,h) for (x,y,w,h) in boxes]
+        ret, current_frame = cap.read()
+        
+        if not ret:
+            break
 
-    #for box in boxes:
-    #    x, y, w, h = box
-    #    model_detections.append((x, y, w, h))
+        frames.append(frame_counter)
+        
+        # Convert the frame to grayscale
+        current_frame_gray = cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY)
+        
+        # Calculate the absolute difference between the current frame and the previous frame
+        frame_diff = cv2.absdiff(current_frame_gray, prev_frame_gray)
+        
+        # Apply thresholding to the difference frame
+        _, thresholded_frame = cv2.threshold(frame_diff, 10, 255, cv2.THRESH_BINARY)
+        
+        # Find contours in the thresholded frame
+        contours, _ = cv2.findContours(thresholded_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        
+        # Draw rectangles around moving objects
+        frame_contours = []
+        for contour in contours:
+            if cv2.contourArea(contour) > 200:  # You can adjust this threshold based on your specific case
+                (x, y, w, h) = cv2.boundingRect(contour)
+                cv2.rectangle(current_frame, (x, y), (x + w, y + h), (255,0, 0), 2)
+                frame_contours.append((x, y, w, h))
 
-        # find average of all detections
-        if len(model_detections) > 0:
-            x = np.mean([x for (x, y, w, h) in model_detections])
-            y = np.mean([y for (x, y, w, h) in model_detections])
-            w = np.mean([w for (x, y, w, h) in model_detections])
-            h = np.mean([h for (x, y, w, h) in model_detections])
-            detections_cords.append((x, y, w, h))
-            frame_detections.append(model_detections)
-            cv2.rectangle(annotated_frame, (int(x) - int(w/2), int(y) - int(h/2)), (int(x) + int(w/2), int(y) + int(h/2)), (0, 255, 255), 2)
-        else: # no model detection
+        # find average of all contours
+        if len(frame_contours) > 0:
+            x = np.mean([x for (x, y, w, h) in frame_contours])
+            y = np.mean([y for (x, y, w, h) in frame_contours])
+            w = np.mean([w for (x, y, w, h) in frame_contours])
+            h = np.mean([h for (x, y, w, h) in frame_contours])
+            motions_cords.append((x, y, w, h))
+            frame_motions.append(frame_contours)
+            cv2.rectangle(current_frame, (int(x), int(y)), (int(x) + int(w), int(y) + int(h)), (0, 255, 0), 2)
+        else: # no motion detection
+            motions_cords.append((0, 0, 0, 0))
+            frame_motions.append([])
+                
+
+        # Run YOLOv8 inference on the frame if motion is detected
+        if len(frame_contours) > 0:
+            results = model(current_frame)
+
+            # annotated frame from model
+            annotated_frame = results[0].plot()
+
+            # get detection 
+            boxes = results[0].boxes.xywh.tolist()
+
+            #track_ids = results[0].boxes.id.int().cpu().tolist()
+
+            # get detection cords
+            model_detections = [(x,y,w,h) for (x,y,w,h) in boxes]
+
+            #for box in boxes:
+            #    x, y, w, h = box
+            #    model_detections.append((x, y, w, h))
+
+            # get class ids
+            labels = results[0].boxes.cls.tolist()
+            class_ids.append(labels)
+
+            # find average of all detections
+            if len(model_detections) > 0:
+                x = np.mean([x for (x, y, w, h) in model_detections])
+                y = np.mean([y for (x, y, w, h) in model_detections])
+                w = np.mean([w for (x, y, w, h) in model_detections])
+                h = np.mean([h for (x, y, w, h) in model_detections])
+                detections_cords.append((x, y, w, h))
+                frame_detections.append(model_detections)
+                cv2.rectangle(annotated_frame, (int(x) - int(w/2), int(y) - int(h/2)), (int(x) + int(w/2), int(y) + int(h/2)), (0, 255, 255), 2)
+            else: # no model detection
+                detections_cords.append((0, 0, 0, 0))
+                frame_detections.append([])
+        else: # no motion detection
             detections_cords.append((0, 0, 0, 0))
             frame_detections.append([])
-    else: # no motion detection
-        detections_cords.append((0, 0, 0, 0))
-        frame_detections.append([])
+
+            # add class ids
+            class_ids.append([])
+            
+
+        # Display the resulting frame
+        if len(frame_contours) > 0:
+            cv2.imshow('Motion Detection & YOLOv8 Inference', annotated_frame)
+            # save annotated frame
+            cv2.imwrite(f"/Users/edwardamoah/Documents/GitHub/BeeVision/solitary_bee_hotels/outputs/video_{frame_counter}.jpg", annotated_frame)
+        else:
+            cv2.imshow('Motion Detection & YOLOv8 Inference', current_frame)
+
+        #cv2.imshow("YOLOv8 Inference", annotated_frame)
         
+        # Update the previous frame
+        #prev_frame_gray = current_frame_gray
+        prev_frame_gray = update_background(current_frame_gray, prev_frame_gray, 0.1)
+        
+        # Break the loop if 'q' key is pressed
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-    # Display the resulting frame
-    if len(frame_contours) > 0:
-        cv2.imshow('Motion Detection & YOLOv8 Inference', annotated_frame)
-        # save annotated frame
-        cv2.imwrite(f"/Users/edwardamoah/Documents/GitHub/BeeVision/solitary_bee_hotels/outputs/video_{frame_counter}.jpg", annotated_frame)
-    else:
-        cv2.imshow('Motion Detection & YOLOv8 Inference', current_frame)
+    # Release the capture object and close all windows
+    cap.release()
+    cv2.destroyAllWindows()
 
-    #cv2.imshow("YOLOv8 Inference", annotated_frame)
+
+    # save results to csv
+    #df = pd.DataFrame({'frame': frames, 'motions': motions_cords, 'detections': detections_cords})
+    df = pd.DataFrame({'frame_number': frames, 'motions_coordinates': motions_cords, 'detections_coordinates': detections_cords, 'detections_classes': class_ids, "frame_motions": frame_motions, "frame_detections": frame_detections})
     
-    # Update the previous frame
-    #prev_frame_gray = current_frame_gray
-    prev_frame_gray = update_background(current_frame_gray, prev_frame_gray, 0.1)
-    
-    # Break the loop if 'q' key is pressed
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# Release the capture object and close all windows
-cap.release()
-cv2.destroyAllWindows()
+    if persist: # if persist is true, save to filename
+        df.to_csv(filename, index=False)
 
 
-# save results to csv
-#df = pd.DataFrame({'frame': frames, 'motions': motions_cords, 'detections': detections_cords})
-df = pd.DataFrame({'frame': frames, 'motions': motions_cords, 'detections': detections_cords, "frame_motions": frame_motions, "frame_detections": frame_detections})
-df.to_csv('results.csv', index=False)
+if __name__ == "__main__":
+    # construct the argument parser and parse the arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--video", default="/Users/edwardamoah/Documents/GitHub/BeeVision/solitary_bee_hotels/datasets/videos/annotated/2023-09-08_10_10_01.mp4", required=False, help="path to input video file")
+    parser.add_argument("--persist", default=True, required=False, help="persist results to csv")
+    parser.add_argument("--filename", default="results.csv", required=False, help="filename to persist results to")
+    args = parser.parse_args()
+    main(args.video, args.persist, args.filename)
